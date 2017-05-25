9d28d77f2c5c0cfe0f8e44910b19efa492fb52a2
Saranya Ganapathy <saranyaganapathy95@gmail.com>
2017-05-24 23:16:24 -0400

    checkpoint 4 assignment complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,6 +18,23 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "#remove_entry" do
+    it "removes an entry using the name, phone_number, and email" do
+      book = AddressBook.new
+      book.add_entry("Kanye West", "123.456.6789", 'kanyewest@gmail.com')
+
+      name = 'Ada Lovelace'
+      phone_number = '010.012.1815'
+      email = 'augusta.king@lovelace.com'
+      book.add_entry(name, phone_number, email)
+
+      expect(book.entries.size).to eq(2)
+      book.remove_entry(name, phone_number, email)
+      expect(book.entries.size).to eq(1)
+      expect(book.entries.first.name).to eq('Kanye West')
+    end
+  end
+
   describe "#add_entry" do
     it "adds only one entry to the address book" do
       book = AddressBook.new
@@ -36,5 +53,4 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq ('augusta.king@lovelace.com')
     end
   end
-
 end
